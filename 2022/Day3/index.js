b5dc412f3a70e2afef7a85b8adbc4c90a954e5dc
fabrice dhuicque <fabrice.dhuicque2021@campus-eni.fr>
2022-12-03 07:34:36 +0100

    2022 - Day 3

--- 2022/Day3/index.js
+++ 2022/Day3/index.js
@@ -3,9 +3,9 @@ let day = function day(){
     /* GLOBAL */
     dayNumber = __dirname.substring(__dirname.lastIndexOf('Day')+3);
     console.log("Day " + dayNumber + " answers:");
-    const test = true;
-    const test1 = 0;
-    const test2 = 0;
+    const test = false;
+    const test1 = 157;
+    const test2 = 70;
 
     const fs = require('fs');
     let dir = test ? __dirname + '/testInput.txt' : __dirname + '/input.txt';
@@ -22,18 +22,58 @@ let day = function day(){
 
     /* SPECIFIC */
 
-    const element = input.split("\r\n\r\n");
-    let indivElem = [];
-
-    element.forEach(elem => {
-        indivElem.push(elem.split("\r\n"));
-    });
+    const element = input.split("\r\n");
+    sacks = []
+    element.forEach(sack => {
+        sacks.push([
+            sack.slice(0, sack.length/2),
+            sack.slice(sack.length/2, sack.length)
+        ]);
+    })
 
     /* PART 1 */
-    let answer1 = undefined;
+    let answer1 = 0;
+    items = [];
+    sacks.forEach(sack => {
+        let filled = false;
+        sack0 = sack[0].split('');
+        sack1 = sack[1].split('');
+        sack0.forEach(item1 => {
+            if(!filled && sack[1].includes(item1)){
+                items.push(item1);
+                filled = true;
+            }
+        })
+    })
+
+    items.forEach(char => {
+        answer1 += char.charCodeAt(0)-96 < 0 ? char.charCodeAt(0) -38 : char.charCodeAt(0)-96;
+    })
 
     /* PART 2 */
-    let answer2 = undefined;
+    let answer2 = 0;
+    let groupOfThree = [];
+    for(let i=0; i<element.length; i+=3){
+        let trio = [];
+        trio.push(element[i]);
+        trio.push(element[i+1]);
+        trio.push(element[i+2]);
+        groupOfThree.push(trio);
+    }
+    badges = [];
+    groupOfThree.forEach(group => {
+        first = group[0].split('');
+        filled = false
+        first.forEach(char => {
+            if(!filled && group[1].includes(char) && group[2].includes(char)){
+                badges.push(char);
+                filled = true;
+            }
+        });
+    })
+    badges.forEach(char => {
+        answer2 += char.charCodeAt(0)-96 < 0 ? char.charCodeAt(0) -38 : char.charCodeAt(0)-96;
+    })
 
     /* RESULTS */
     if(answer1 === undefined && answer2 === undefined){
